587f1e434a57da399257237be900a09107772765
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-08-28 21:38:48 +0100

    try and enable some request logging

--- src/main/java/com/github/willpdp/springboot/configuration/MicroserviceConfiguration.java
+++ src/main/java/com/github/willpdp/springboot/configuration/MicroserviceConfiguration.java
@@ -1,7 +1,19 @@
 package com.github.willpdp.springboot.configuration;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootConfiguration
 public class MicroserviceConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter getCommonsRequestLoggingFilter() {
+        final CommonsRequestLoggingFilter commonsRequestLoggingFilter = new CommonsRequestLoggingFilter();
+        commonsRequestLoggingFilter.setIncludeClientInfo(true);
+        commonsRequestLoggingFilter.setIncludePayload(false);
+        commonsRequestLoggingFilter.setIncludeQueryString(true);
+        commonsRequestLoggingFilter.setIncludeHeaders(false);
+        return commonsRequestLoggingFilter;
+    }
 }
